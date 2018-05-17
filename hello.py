7d47f6dbd699e60ef4221c374a49bfa46367df92
andersbando-hess <bandersman@gmail.com>
2018-05-17 14:44:25 -0400

    finsihed workship

--- hello.py
+++ hello.py
@@ -2,8 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "Flask App!"
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
+
+@app.route("/hello/<string:name>/")
+def getMember(name):
+    return "Hello " + name + "!"
 
 if __name__ == "__main__":
     app.run()
